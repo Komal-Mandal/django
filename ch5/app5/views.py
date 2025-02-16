53c180995ddbfb870753e466d94995e6dbbe1daf
Komal Mandal <komalmandal1907@gmail.com>
2025-02-16 19:18:18 +0530

    fourt commit

--- ch5/app5/views.py
+++ ch5/app5/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from . import views
 
-def learn_django(req):
-    return render(req,"app5/django.html")
+# variable example 1
+"""def  learn_django(req):
+    return render(req,"app5/django.html",context={"name":"django"} )"""
+
+# variable example 2
+'''def  learn_django(req):
+
+    name = "django"
+    duration = "4 months"
+    seats = 10
+    detail = {"nm":name, "du":duration,"st":seats}
+
+    return render(req,"app5/django.html",detail )'''
+
+# example 2 - Filters
+
+def  learn_django(req):
+    return render(req,"app5/django.html",context={"name":"django", "desc":"django is the awesome web programming"} )
+
